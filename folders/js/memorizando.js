f9e879b8e3b0faf50f8fbeca691228d8027b8a53
BrahiamCastillo <mymoviehb@gmail.com>
2020-10-08 00:42:11 -0400

    Falta aleatorio

--- folders/js/memorizando.js
+++ folders/js/memorizando.js
@@ -1,39 +1,42 @@
 $(document).ready(function () {
     nuevo();
-    var img1;
+    var img1=null;
     var img2;
     var contracha=0;
     var contvidas=0;
     var contpuntos=0;
     
     $(".sobre").click(function() {
-        img1 = $(".col-md-2").each(function() {
-            $(this).find("img").eq(0).attr("src");
-        })
-        alert(img1);
-        $(this).remove();
-        $(".sobre").click(function() {
-            img2 = $(this).attr("src");
-            if(img1===img2) {
-                alert(img2);
-                $(this).remove();
+        if(img1==null) {
+            img1 = $(this).siblings().attr("src");
+            alert(img1);
+            $(this).remove();
+        } else {
+            img2 = $(this).siblings().attr("src");
+            alert(img2);
+            $(this).remove();
+            if(img1==img2) {
                 contracha++;
                 contpuntos=contpuntos+2;
                 $("#racha").html(contracha);
                 $("#puntos").html(contpuntos)
                 toastr.success("¡Has acertado!.");
+                img1=null;
+                img2=null;
             } else {
-                $(this).remove();
                 contvidas--;
                 contpuntos=contpuntos-2;
                 $("#puntos").html(contpuntos);
                 $("#vida").html(contvidas);
                 toastr.error("¡Perdiste!.");
+                img1=null;
+                img2=null;
             }
-        })
+        }
     })
     
 });
+
 
 function nuevo() {
     toastr.success("Debes seleccionar dos casillas incógnitas, si das con dos imágenes iguales, sumas puntos, de lo contrario pierdes.");
