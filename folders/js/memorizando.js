f9480e124505d6a2e87964c1b8b9d20c4ce7266c
BrahiamCastillo <mymoviehb@gmail.com>
2020-10-08 16:00:41 -0400

    Hecho completo

--- folders/js/memorizando.js
+++ folders/js/memorizando.js
@@ -1,18 +1,36 @@
 $(document).ready(function () {
+    toastr.success("Debes seleccionar dos casillas incógnitas, si das con dos imágenes iguales, sumas puntos, de lo contrario pierdes, si llegas a 6 puntos, ganas automáticamente.");
     nuevo();
     var img1=null;
     var img2;
     var contracha=0;
-    var contvidas=6;
+    var contvidas=4;
     var contpuntos=0;
+    var miliSegundos=0;
+    var tiempoCorriendo=null;
+    var imagen1;
+    var imagen2;
+
+    $(".sobre").css("opacity", 0.0);
+
+    tiempoCorriendo=setInterval(function() {
+        miliSegundos++;
+        if(miliSegundos>300) {
+            $(".sobre").css("opacity", 1.0);
+
+        }
+    })
+
     
     $(".sobre").click(function() {
         if(img1==null) {
             img1 = $(this).siblings().attr("src");
-            $(this).remove();
+            imagen1=$(this);
+            $(this).hide();
         } else {
             img2 = $(this).siblings().attr("src");
-            $(this).remove();
+            imagen2=$(this);
+            $(this).hide();
             if(img1==img2) {
                 contracha++;
                 contpuntos=contpuntos+2;
@@ -21,6 +39,7 @@ $(document).ready(function () {
                 toastr.success("¡Has acertado!.");
                 img1=null;
                 img2=null;
+
             } else {
                 contvidas--;
                 if(contvidas<0) {
@@ -38,6 +57,9 @@ $(document).ready(function () {
                 $("#vida").html(contvidas);
                 $("#racha").html(contracha);
                 toastr.warning("¡Has fallado, esfuérzate mas!.");
+                $("#puntos").html(contpuntos);
+                imagen1.show(2000);
+                imagen2.show(2000);
                 if(contvidas==0) {
                     toastr.error('¡Has perdido!, presione "Nuevo" para una nueva partida.')
                     $(".sobre").remove();
@@ -59,7 +81,6 @@ $(document).ready(function () {
 
 
 function nuevo() {
-    toastr.success("Debes seleccionar dos casillas incógnitas, si das con dos imágenes iguales, sumas puntos, de lo contrario pierdes, si llegas a 6 puntos, ganas automáticamente.");
     var random;
     var imagenes=[
         "IMG/1.png", "IMG/2.png",
